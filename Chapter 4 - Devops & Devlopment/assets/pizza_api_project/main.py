30d80c1adc1a7ecc8822771d5ffcb4b3d225473d
DanielWidawski <daniel.widawski06@gmail.com>
2026-06-15 17:50:51 +0300

    config with pydantic settings

--- Chapter 4 - Devops & Devlopment/assets/pizza_api_project/main.py
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/main.py
@@ -1,8 +1,10 @@
 import uvicorn
 
 from api_init.fastapi_init import init_api_app
+from settings.api_settings import get_config
 
 app = init_api_app()
+config = get_config()
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host=config.host, port=config.port)
---
+++ Chapter 4 - Devops & Devlopment/assets/pizza_api_project/settings/api_settings.py
@@ -0,0 +1,18 @@
+from functools import lru_cache
+
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class ApiConfig(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+    host: str = "localhost"
+    port: int = 8000
+
+
+@lru_cache
+def get_config() -> ApiConfig:
+    return ApiConfig()
